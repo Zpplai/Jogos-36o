4c76251b357e96c8534921a91c2ed24c15ce3870
Zpplai <156550483+Zpplai@users.noreply.github.com>
2026-04-04 19:02:22 -0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -25,3 +25,42 @@ function carregarAds() {
     });
 }
 
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIH9HELH9xeKlkGKgCHeijL2-47Y1D8yY",
+  authDomain: "xbox-b9d32.firebaseapp.com",
+  projectId: "xbox-b9d32",
+  messagingSenderId: "708796067849",
+  appId: "1:708796067849:web:2c64c21af8b094d4bac8d8"
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+// SUA VAPID KEY
+const VAPID_KEY = "BHH2CPxseD-v4JkRytM0RseK4FEkijcoSY6p14Axk09VfszPAvTDDp1yS4QN8jOcZKwwsTD5UsF6zK4kxZnbSz0";
+
+// ATIVA NOTIFICAÇÃO
+async function ativarNotificacao() {
+  try {
+    const permission = await Notification.requestPermission();
+
+    if (permission === "granted") {
+      const token = await getToken(messaging, {
+        vapidKey: VAPID_KEY
+      });
+
+      console.log("TOKEN:", token);
+      alert("Notificação ativada 🔔");
+    }
+
+  } catch (err) {
+    console.log("Erro:", err);
+  }
+}
+
+// ativa no primeiro clique
+document.addEventListener("click", ativarNotificacao, { once: true });
