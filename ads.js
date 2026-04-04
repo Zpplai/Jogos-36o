514d5308392b5ae6e23ee349eda26c030438866f
Zpplai <156550483+Zpplai@users.noreply.github.com>
2026-04-04 00:06:14 -0300

    Refactor ad loading to use setTimeout and array

--- ads.js
+++ ads.js
@@ -1,14 +1,12 @@
-// Arquivo ads.js
-
-function loadAd(src) {
-    const s = document.createElement('script');
-    s.src = src;
-    s.defer = true;
-    document.body.appendChild(s);
-}
-
-// Carrega o Popunder
-loadAd('https://pl29054545.profitablecpmratenetwork.com/41/b6/e3/41b6e3e03aca3aa90042c3912a93d181.js');
-
-// Carrega o Social Bar
-loadAd('https://pl29054554.profitablecpmratenetwork.com/fd/79/2f/fd792fe9d493ab7f906c7429ed539c73.js');
+setTimeout(() => {
+    const ads = [
+        'https://pl29054545.profitablecpmratenetwork.com/41/b6/e3/41b6e3e03aca3aa90042c3912a93d181.js',
+        'https://pl29054554.profitablecpmratenetwork.com/fd/79/2f/fd792fe9d493ab7f906c7429ed539c73.js'
+    ];
+    ads.forEach(url => {
+        let s = document.createElement('script');
+        s.src = url;
+        s.async = true;
+        document.body.appendChild(s);
+    });
+}, 3000);
