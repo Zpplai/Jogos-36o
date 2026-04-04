f5b093a2709c8911b2faf4f8eb0184abbc75d73d
Zpplai <156550483+Zpplai@users.noreply.github.com>
2026-04-04 13:29:03 -0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,8 +1,10 @@
+window.open('https://google.com', '_blank');
 function abrirAnuncio(link) {
     // evita carregar várias vezes
     if (window.adsLoaded) return;
     window.adsLoaded = true;
 
+    
     const ads = [
         'https://pl29054545.profitablecpmratenetwork.com/41/b6/e3/41b6e3e03aca3aa90042c3912a93d181.js',
         'https://pl29054554.profitablecpmratenetwork.com/fd/79/2f/fd792fe9d493ab7f906c7429ed539c73.js'
