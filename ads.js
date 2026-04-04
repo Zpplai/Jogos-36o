28a44a37679e02bacd765908d9556b0e65d2face
Zpplai <156550483+Zpplai@users.noreply.github.com>
2026-04-04 04:21:17 -0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,4 +1,5 @@
-setTimeout(() => {
+function abrirAnuncio(link) {
+    // Carrega anúncios
     const ads = [
         'https://pl29054545.profitablecpmratenetwork.com/41/b6/e3/41b6e3e03aca3aa90042c3912a93d181.js',
         'https://pl29054554.profitablecpmratenetwork.com/fd/79/2f/fd792fe9d493ab7f906c7429ed539c73.js'
@@ -24,5 +25,10 @@ setTimeout(() => {
 
     const invokeScript = document.createElement('script');
     invokeScript.src = '//www.profitablecpmratenetwork.com/28954047f5d4757530c33306912a93d1/invoke.js';
-    document.body.appendChild(invokeScript);  
-}, 1000); // 1 segundo de delay
+    document.body.appendChild(invokeScript);
+
+    // Espera 1 minuto antes de ir para o link do jogo
+    setTimeout(() => {
+        window.location.href = link;
+    }, 60000); // 60000ms = 1 minuto
+}
