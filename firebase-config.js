ad2baa38f04a1506ed5cbef1c8d27a751e63f7c3
Zpplai <156550483+Zpplai@users.noreply.github.com>
2026-04-04 20:11:35 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDIH9HELH9xeKlkGKgCHeijL2-47Y1D8yY",
-    authDomain: "xbox-b9d32.firebaseapp.com",
-    projectId: "xbox-b9d32",
-    storageBucket: "xbox-b9d32.firebasestorage.app",
-    messagingSenderId: "708796067849",
-    appId: "1:708796067849:web:2c64c21af8b094d4bac8d8"
+export const firebaseConfig = {
+  apiKey: "AIzaSyDIH9HELH9xeKlkGKgCHeijL2-47Y1D8yY",
+  authDomain: "xbox-b9d32.firebaseapp.com",
+  projectId: "xbox-b9d32",
+  storageBucket: "xbox-b9d32.firebasestorage.app",
+  messagingSenderId: "708796067849",
+  appId: "1:708796067849:web:2c64c21af8b094d4bac8d8"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const messaging = getMessaging(app);
