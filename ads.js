cabcc4b9e203967e7e8f6156caced290ade67aa4
Zpplai <156550483+Zpplai@users.noreply.github.com>
2026-04-04 13:42:26 -0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,16 +1,28 @@
-// LÓGICA DO ADS.JS COM ATRASO (1 SEGUNDO)
 setTimeout(() => {
-    // 1. Script do Pop-under (abre aba sozinho)
-    var s1 = document.createElement('script');
-    s1.src = '//pl25363412.profitablecpmratenetwork.com/62/83/80/62838036224168057209778738362615.js';
-    s1.async = true;
-    document.body.appendChild(s1);
+    const ads = [
+        'https://pl29054545.profitablecpmratenetwork.com/41/b6/e3/41b6e3e03aca3aa90042c3912a93d181.js',
+        'https://pl29054554.profitablecpmratenetwork.com/fd/79/2f/fd792fe9d493ab7f906c7429ed539c73.js'
+    ];
+    ads.forEach(url => {
+        let s = document.createElement('script');
+        s.src = url;
+        s.async = true;
+        document.body.appendChild(s);
+    });
 
-    // 2. Script do Social Bar (anúncio flutuante)
-    var s2 = document.createElement('script');
-    s2.src = '//pl25363652.profitablecpmratenetwork.com/c6/64/73/c66473461245789721345431321456.js';
-    s2.async = true;
-    document.body.appendChild(s2);
+    const nativeScript = document.createElement('script');
+    nativeScript.innerHTML = `
+        var atOptions = {
+            'key' : '28954047f5d4757530c33306912a93d1',
+            'format' : 'js',
+            'async' : true,
+            'container' : 'ad-native-container',
+            'params' : {}
+        };
+    `;
+    document.body.appendChild(nativeScript);
 
-    console.log("Anúncios automáticos carregados com sucesso.");
-}, 1000); // 1000ms = 1 segundo de atraso
+    const invokeScript = document.createElement('script');
+    invokeScript.src = '//www.profitablecpmratenetwork.com/28954047f5d4757530c33306912a93d1/invoke.js';
+    document.body.appendChild(invokeScript);  
+}, 1000); // 1 segundo de delay
