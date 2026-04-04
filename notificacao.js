6f09d89c1a59aca940b8983bfccfd8c1acb71fac
Zpplai <156550483+Zpplai@users.noreply.github.com>
2026-04-04 19:12:00 -0300

    Update notificacao.js

--- notificacao.js
+++ notificacao.js
@@ -1,11 +1,44 @@
-// Função exportada para ser usada no app.js
-export function enviarAviso(nomeJogo) {
-    if (Notification.permission === "granted") {
-        new Notification("🎮 Novo Jogo na Área!", {
-            body: `O jogo ${nomeJogo} foi adicionado! Vem conferir.`,
-            icon: "https://th.bing.com/th/id/R.7088b99187308736d9333908E6659682?pid=ImgRaw&r=0"
-        });
-    } else if (Notification.permission !== "denied") {
-        Notification.requestPermission();
+// FIREBASE NOTIFICAÇÃO
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIH9HELH9xeKlkGKgCHeijL2-47Y1D8yY",
+  authDomain: "xbox-b9d32.firebaseapp.com",
+  projectId: "xbox-b9d32",
+  messagingSenderId: "708796067849",
+  appId: "1:708796067849:web:2c64c21af8b094d4bac8d8"
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+const VAPID_KEY = "BHH2CPxseD-v4JkRytM0RseK4FEkijcoSY6p14Axk09VfszPAvTDDp1yS4QN8jOcZKwwsTD5UsF6zK4kxZnbSz0";
+
+// registra service worker
+navigator.serviceWorker.register('/firebase-messaging-sw.js')
+.then(() => {
+    console.log("Service Worker registrado");
+})
+.catch(err => {
+    console.log("Erro SW:", err);
+});
+
+// ativa notificação no clique
+document.addEventListener("click", async () => {
+    try {
+        const permission = await Notification.requestPermission();
+
+        if (permission === "granted") {
+            const token = await getToken(messaging, {
+                vapidKey: VAPID_KEY
+            });
+
+            console.log("TOKEN:", token);
+            alert("Notificação ativada 🔔");
+        }
+
+    } catch (err) {
+        console.log("Erro:", err);
     }
-}
+}, { once: true });
