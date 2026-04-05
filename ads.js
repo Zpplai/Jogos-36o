157e34b44fdd979fcba79ab73c9118c64b5b2ab8
Zpplai <156550483+Zpplai@users.noreply.github.com>
2026-04-05 17:08:27 -0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,4 +1,5 @@
-// Configuração do Banner Nativo
+// 1. Configuração do Banner Nativo
+// Certifique-se de que existe um <div id="ad-native-container"></div> no seu HTML
 window.atOptions = {
     'key' : '28954047f5d4757530c33306912a93d1',
     'format' : 'js',
@@ -7,7 +8,7 @@ window.atOptions = {
     'params' : {}
 };
 
-// Função para injetar os anúncios de Pop/Redirecionamento
+// 2. Função para injetar os scripts de anúncios (Pop-under e Invoke)
 function carregarAds() {
     const scripts = [
         'https://pl29054545.profitablecpmratenetwork.com/41/b6/e3/41b6e3e03aca3aa90042c3912a93d181.js',
@@ -16,6 +17,7 @@ function carregarAds() {
     ];
 
     scripts.forEach(url => {
+        // Verifica se o script já não foi injetado para evitar duplicidade
         if (!document.querySelector(`script[src^="${url}"]`)) {
             const s = document.createElement('script');
             s.src = url;
@@ -25,42 +27,5 @@ function carregarAds() {
     });
 }
 
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDIH9HELH9xeKlkGKgCHeijL2-47Y1D8yY",
-  authDomain: "xbox-b9d32.firebaseapp.com",
-  projectId: "xbox-b9d32",
-  messagingSenderId: "708796067849",
-  appId: "1:708796067849:web:2c64c21af8b094d4bac8d8"
-};
-
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-
-// SUA VAPID KEY
-const VAPID_KEY = "BHH2CPxseD-v4JkRytM0RseK4FEkijcoSY6p14Axk09VfszPAvTDDp1yS4QN8jOcZKwwsTD5UsF6zK4kxZnbSz0";
-
-// ATIVA NOTIFICAÇÃO
-async function ativarNotificacao() {
-  try {
-    const permission = await Notification.requestPermission();
-
-    if (permission === "granted") {
-      const token = await getToken(messaging, {
-        vapidKey: VAPID_KEY
-      });
-
-      console.log("TOKEN:", token);
-      alert("Notificação ativada 🔔");
-    }
-
-  } catch (err) {
-    console.log("Erro:", err);
-  }
-}
-
-// ativa no primeiro clique
-document.addEventListener("click", ativarNotificacao, { once: true });
+// 3. Executa o carregamento assim que o script for lido
+carregarAds();
